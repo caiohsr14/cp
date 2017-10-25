2121e23200c9aee7d97f27493f8fb8beed7a02f8
Caio Rodrigues <caio.rodrigues@nuti.ic.ufmt.br>
2017-10-25 18:53:05 -0300

    update 25/10/2017

--- uva/10114.py
+++ uva/10114.py
@@ -1,19 +1,16 @@
 s = [float(x) for x in input().split()]
 while s[0] >= 0.0:
     z = 101 * [0.0]
-    t = s[2]
-    d = t / s[0]
+    d = s[2] / s[0]
     m = 0
     for i in range(int(s[3])):
         a = input().split()
         z[int(a[0])] = float(a[1])
-    p = z[0]
-    c = s[2] + s[1]
-    c -= c * p
-    while t >= c:
-        t -= d
+    c = s[1] + s[2] - (s[1] + s[2]) * z[0]
+    while s[2] >= c:
+        s[2] -= d
         m += 1
-        p = z[m] if z[m] != 0.0 else p
-        c -= c * p
+        z[0] = z[m] if z[m] != 0.0 else z[0]
+        c -= c * z[0]
     print(m, "month" if m == 1 else "months")
     s = [float(x) for x in input().split()]
